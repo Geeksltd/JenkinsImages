073917657c17323960b9e6a30b4d9aa8d53d3b81
G\Matt <matt@geeks.ltd.uk>
2019-01-24 16:07:38 +0000

    no message

--- RunJenkins.ps1
+++ RunJenkins.ps1
@@ -1,9 +1,9 @@
 docker run -it --rm -p 8080:8080 `
---env DOCKER_HOST=(Test-Connection -ComputerName $env:computername -count 1).IPV4Address.ipaddressTOstring + ":2375" `
+--env DOCKER_HOST=$("tcp://" + (Test-Connection -ComputerName $env:computername -count 1).IPV4Address.ipaddressTOstring + ":2375") `
 --env PROJECT_JENKINS_FILE_GIT_URL=https://github.com/Geeksltd/MasterJenkinsfile.git `
 --env BRANCH=[#BRANCH#] `
 --env PROJECT=[#PROJECT#] `
---env JENKINS_ADMIN_PASSWORD=(New-Guid).ToString() `
+--env JENKINS_ADMIN_PASSWORD=$((New-Guid).ToString()) `
 --env PROJECT_REPOSITORY_URL=[#PROJECT_REPOSITORY_URL#] `
 --env PROJECT_REPOSITORY_USERNAME=[#PROJECT_REPOSITORY_USERNAME#] `
 --env PROJECT_REPOSITORY_PASSWORD=[#PROJECT_REPOSITORY_PASSWORD#]
