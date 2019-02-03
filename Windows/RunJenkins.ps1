822f5d37ce83f25fd02563db9499a2281b392bf3
G\Matt <matt@geeks.ltd.uk>
2019-02-03 13:37:44 +0000

    Added the script for running Jenkins.

---
+++ Windows/RunJenkins.ps1
@@ -0,0 +1,19 @@
+$Credentials=(aws sts assume-role --role-arn [#BuildServerAgent_ROLE_ARN#] --role-session-name [#PROJECT_NAME#] | ConvertFrom-Json | select -expand Credentials)
+$AccessKeyId=$Credentials.AccessKeyId
+$SecretAccessKey=$Credentials.SecretAccessKey
+$SessionToken=$Credentials.SessionToken
+docker run -it --rm -p 8080:8080 `
+--env DOCKER_HOST=$("tcp://"+(Test-Connection -ComputerName $env:computername -count 1).IPV4Address.ipaddressTOstring + ":2375") `
+--env PROJECT_JENKINS_FILE_GIT_URL=https://github.com/Geeksltd/MasterJenkinsfile.git `
+--env BRANCH=[#BRANCH#] `
+--env PROJECT=[#PROJECT#] `
+--env PROJECT_TASK_FAMILY_NAME=[#PROJECT_TASK_FAMILY_NAME#] `
+--env JENKINS_ADMIN_PASSWORD=$((New-Guid).ToString()) `
+--env PROJECT_REPOSITORY_URL=[#PROJECT_REPOSITORY_URL#] `
+--env PROJECT_REPOSITORY_USERNAME=[#PROJECT_REPOSITORY_USERNAME#] `
+--env PROJECT_REPOSITORY_PASSWORD="[#PROJECT_REPOSITORY_PASSWORD#]" `
+--env CONTIANER_REPOSITORY_URL=[#CONTIANER_REPOSITORY_URL#] `
+--env AWS_ACCESS_KEY_ID=$AccessKeyId `
+--env AWS_SECRET_ACCESS_KEY=$SecretAccessKey `
+--env AWS_SESSION_TOKEN=$SessionToken `
+geeksltd/jenkins-windows:1.03
