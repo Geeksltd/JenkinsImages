c814ebd66822d196e8a4da4c5d88c9f8c35c6dfd
G\Matt <matt@geeks.ltd.uk>
2019-02-25 16:15:33 +0000

    Added the powershell profile scripts.

---
+++ Windows/JenkinsMetadata/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,27 @@
+﻿function getLastSuccessfulDeploymentCommitDate(){
+[alias("GetLastDeploymentCommit")]
+
+$lastDeploymentTagDate=git tag --contains="Test-Pangolin" --sort=-taggerdate --format="%(taggerdate:iso)" | Select-Object -first 1;
+if($LASTEXITCODE ==129)
+	$lastDeploymentTagDate = [datetime].MinValue;
+
+return [datetime]::Parse($lastDeploymentTagDate);
+}
+
+function getDBChangeScriptsSince($lastDeploymentDate)
+{
+	foreach($file in Get-ChildItem DB)
+	{
+		$fileDate = getDateFromFileName($file);
+		if($fileDate != null && ($fileDate -lt @lastDeploymentDate))
+		$file
+	}
+}
+
+
+function getDateFromFileName($fileName)
+{
+	try{
+	}
+	
+}
