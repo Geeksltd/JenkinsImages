02a2666f9139233d2c90326e3ad554fbe3305733
G\Matt <matt@geeks.ltd.uk>
2019-02-26 15:50:12 +0000

    no message

--- Windows/JenkinsMetadata/Powershell/SQL/backup.ps1
+++ Windows/JenkinsMetadata/Powershell/SQL/backup.ps1
@@ -1,1 +1,19 @@
-ECHO is on.
+function backupDatabase()
+{
+    param(
+        [Parameter(mandatory=$true)][string]$databaseName,
+        [Parameter(mandatory=$true)][string]$s3_object_arn_to_backup_to,
+        [Parameter(mandatory=$true)][string]$databaseServer,
+        [Parameter(mandatory=$true)][string]$databaseUsername,
+        [Parameter(mandatory=$true)][string]$databasePassword,
+        )
+
+    $command = "exec msdb.dbo.rds_backup_database 
+                @source_db_name='$databaseName', 
+                @s3_arn_to_backup_to='$s3_object_arn_to_backup_to',
+                @overwrite_S3_backup_file=1,
+                @type='FULL';"
+
+
+    Invoke-Sqlcmd -ServerInstance $databaseServer -Database $databaseName -Username $databaseUsername -Password $databasePassword -Query $command  -AbortOnError
+}
--- Windows/JenkinsMetadata/Powershell/SQL/restore.ps1
+++ Windows/JenkinsMetadata/Powershell/SQL/restore.ps1
@@ -1,1 +1,13 @@
-ECHO is on.
+function restoreDatabase()
+{
+    param(
+        [Parameter(mandatory=$true)][string]$databaseName,
+        [Parameter(mandatory=$true)][string]$s3_object_arn_to_restore_from        
+        )
+
+    $command = "exec msdb.dbo.rds_restore_database 
+                @restore_db_name='$databaseName', 
+                @s3_arn_to_restore_from='$s3_object_arn_to_restore_from';"
+
+    Invoke-Sqlcmd -ServerInstance $databaseServer -Database $databaseName -Username $databaseUsername -Password $databasePassword -Query $command
+}
