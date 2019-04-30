ba77b868ac10d6b79bd924eb3de67fbbadbc9540
G\Matt <matt@geeks.ltd.uk>
2019-04-30 15:12:19 +0100

    geeksltd/jenkins-windows:1.18 Added updating runtime environment variables.

---
+++ Windows/Powershell/Scripts/AWS/ECS/registerNewTaskRevision.ps1
@@ -0,0 +1,38 @@
+function registerNewTaskRevision()
+{
+     param(
+        [Parameter(mandatory=$true)][string]$newImage,
+        [string]$taskName=$env:PROJECT_TASK_FAMILY_NAME,
+        [string]$region=$env:REGION,
+        [System.Collections.Hashtable]$environmentVariables
+        )
+    
+    $taskinfo= aws ecs describe-task-definition --task-definition $taskName --region $region --profile admin-uat | ConvertFrom-Json;
+    $newTaskDefinition=$taskinfo.taskDefinition[0] | Select-Object -Property * -ExcludeProperty status,compatibilities,taskDefinitionArn,requiresAttributes,revision;
+    $newTaskDefinition.containerDefinitions[0].image=$newImage;   
+    $envsDic = ConvertToEnvironmentVariables($environmentVariables);    
+    $newTaskDefinition.containerDefinitions[0].environment=$envsDic;
+    $jsonArg = $newTaskDefinition | ConvertTo-Json -Depth 10;
+
+    $newTaskDefinition = aws ecs register-task-definition --region $region --family $taskName --cli-input-json $($jsonArg.Replace("`"","\`"")) --profile admin-uat
+    return ($newTaskDefinition | ConvertFrom-Json).taskDefinition.taskDefinitionArn;
+}
+
+function ConvertToEnvironmentVariables([System.Collections.Hashtable] $environmentVariables)
+{
+    $result = New-Object System.Collections.Generic.List[System.Object];
+    
+    if($environmentVariables){      
+    $environmentVariables.GetEnumerator() | ForEach-Object { $item = ToPsObject($_); $result.Add($item) }   
+    }
+
+    return ,$result.ToArray();
+}
+
+function ToPsObject($keyValuePair)
+{   
+    $obj = New-Object -TypeName PSObject;
+    $obj | Add-Member -MemberType NoteProperty -Name name -Value $keyValuePair.Key
+    $obj | Add-Member -MemberType NoteProperty -Name value -Value $keyValuePair.Value           
+    return $obj;
+}
