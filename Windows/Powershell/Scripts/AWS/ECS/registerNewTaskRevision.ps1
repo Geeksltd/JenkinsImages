ea15c12f3e7c59952ef5bbbbddb82dfb43546a29
G\Matt <matt@geeks.ltd.uk>
2019-05-14 17:40:21 +0100

    1.21

--- Windows/Powershell/Scripts/AWS/ECS/registerNewTaskRevision.ps1
+++ Windows/Powershell/Scripts/AWS/ECS/registerNewTaskRevision.ps1
@@ -3,30 +3,16 @@ function registerNewTaskRevision()
      param(
         [Parameter(mandatory=$true)][string]$newImage,
         [string]$taskName=$env:PROJECT_TASK_FAMILY_NAME,
-        [string]$region=$env:REGION,
-        [System.Collections.Hashtable]$environmentVariables
+        [string]$region=$env:REGION        
         )
     
     $taskinfo= aws ecs describe-task-definition --task-definition $taskName --region $region | ConvertFrom-Json;
     $newTaskDefinition=$taskinfo.taskDefinition[0] | Select-Object -Property * -ExcludeProperty status,compatibilities,taskDefinitionArn,requiresAttributes,revision;
     $newTaskDefinition.containerDefinitions[0].image=$newImage;   
-    $envsDic = ConvertToEnvironmentVariables($environmentVariables);    
-    $newTaskDefinition.containerDefinitions[0].environment=$envsDic;
     $jsonArg = $newTaskDefinition | ConvertTo-Json -Depth 10;
 
     $newTaskDefinition = aws ecs register-task-definition --region $region --family $taskName --cli-input-json $($jsonArg.Replace("`"","\`""))
     return ($newTaskDefinition | ConvertFrom-Json).taskDefinition.taskDefinitionArn;
-}
-
-function ConvertToEnvironmentVariables([System.Collections.Hashtable] $environmentVariables)
-{
-    $result = New-Object System.Collections.Generic.List[System.Object];
-    
-    if($environmentVariables){      
-    $environmentVariables.GetEnumerator() | ForEach-Object { $item = ToPsObject($_); $result.Add($item) }   
-    }
-
-    return ,$result.ToArray();
 }
 
 function ToPsObject($keyValuePair)
